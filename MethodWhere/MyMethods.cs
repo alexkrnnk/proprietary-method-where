d73bd93db2c069119a8bf30096dea63bebe1b0b9
Olexander <73075612+alexkrnnk@users.noreply.github.com>
2022-12-22 09:53:36 +0200

    Method 'MyWhere' realization

---
+++ MethodWhere/MyMethods.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCollection
+{
+    public static class MyMethods
+    {
+        public static IEnumerable<T> MyWhere<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+        {
+            foreach (var item in source)
+            {
+                if (predicate(item))
+                {
+                    yield return item;
+                }
+            }
+        } 
+    }
+}
